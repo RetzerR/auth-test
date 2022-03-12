5cae3062da8366089846b0ede652a24dd14cf5e0
Retzer <101085106+RetzerR@users.noreply.github.com>
2022-03-12 00:39:20 +0000

    First Working Version. this adds the functionality for the cookies, logout/login functionality

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from replit import db
 app = Flask('app')
+
+try:
+    db["users"]
+except KeyError:
+    db["users"] = {}
+
+@app.route("/user/")
+def user():
+    db["users"][request.args.get("username")] = request.args.get("password")
+    return redirect("/")
 
 @app.route('/')
 def hello_world():
-  return '<h1>Hello, World!</h1>'
+  return render_template("index.html")
+
+@app.route("/logout/")
+def logout():
+    return render_template("logout.html", user=request.args.get("user"))
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username in db["users"]:
+        if password == db["users"][username]:
+            return render_template("login.html", user=username)
+        else:
+            return "wrong password"
+    else:
+        return "no user found"
 
 app.run(host='0.0.0.0', port=8080)
